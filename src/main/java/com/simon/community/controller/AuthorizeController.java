bff498c598ac8de8b9f96f637e272fca70b4ed89
SimonTan92 <nixin111000@163.com>
2020-05-04 12:51:31 +0800

    add cookie & session

--- src/main/java/com/simon/community/controller/AuthorizeController.java
+++ src/main/java/com/simon/community/controller/AuthorizeController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -25,7 +28,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callBack(
             @RequestParam(name = "code") String code,
-            @RequestParam(name = "state") String state) {
+            @RequestParam(name = "state") String state,
+            HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientID);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -34,8 +38,14 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null) {
+            // 登录成功，写cookie和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            // 登录失败，重新登录
+            return "redirect:/";
+        }
     }
 
 
